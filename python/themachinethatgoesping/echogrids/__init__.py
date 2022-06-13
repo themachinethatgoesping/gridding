e9d41c0d8318290fa8975c175b2a0f143d020985
Peter Urban <peter.urban@ugent.be>
2022-06-13 10:02:19 +0200

    removed echogrids namespace

--- python/themachinethatgoesping/echogrids/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
-#
-# SPDX-License-Identifier: MPL-2.0
-
-#folders
-from . import functions
-
-#modules
-from .echogrid import EchoGrid
--- python/themachinethatgoesping/echogrids/functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import gridfunctions
-from . import helperfunctions
